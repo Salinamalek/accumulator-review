94c1125f2345a8956a5b25558f71684b05e5cd9e
Salinamalek <salinamalek@pursuit.org>
2022-06-30 11:38:26 -0400

    first two functions completed

--- index.js
+++ index.js
@@ -11,6 +11,45 @@
  * 
  */
 
+function numOfOccurences(str){
+  // Guard Clause
+  //str could not be a string
+  //str length could be zero
+  if(typeof str !== "string" || str.length === 0){
+    return "Error"   
+  }
+
+  //initialize accumulator
+  let accumulator = {}
+
+  //our logic goes here
+  //iterate over the string
+  for (let i = 0; i < str.length; i++){
+    //make a variable to hold each letter as we iterate
+    const letter = str[i].toLowerCase()
+
+    //if the letter does not exist in our accumulator
+    if(!accumulator[letter]){
+        // add the letter to the accumulator
+        accumulator[letter] = 1
+    } else {
+      // add one to the letter in the accumulator
+      accumulator[letter] += 1
+    }
+  }
+
+  // return accumulator
+  return accumulator
+}
+
+// TEST
+// console.log(numOfOccurences("AccabbabbCcbcbB"))
+// console.log(numOfOccurences(35))
+// console.log(numOfOccurences(""))
+
+const myLetters = numOfOccurences("AccabbabbCcbcbB")
+// console.log(myLetters)
+
 /**
  * targetLetters()
  * ----------------
@@ -26,7 +65,30 @@
  * 
  */
 
+function targetLetters(letters, target){
+  //initialize accumulator
+  let accumulator = []
 
+
+  // our logic goes here
+  for (const key in letters) {
+    //prints out each key
+    // console.log("Key:", key)
+    //prints out the value for each key
+    // console.log("Value:", letters[key])
+
+    //if statement that checks if letters[key] > 3
+    if(letters[key] > target){
+      //add the letter to the accumulator
+      //pushes key to the accumulator
+      accumulator.push(key)
+    } 
+  }
+  // return accumulator
+  return accumulator
+}
+// TEST
+// console.log(targetLetters(myLetters, 3))
 
 /**
  * getCartTotal()
@@ -45,3 +107,142 @@
     getCartTotal(cart);
  *  //> "$30.00"
  */
+
+
+
+    // NOTES 
+
+  //   function calculateTotal(rolls){
+  //     let count = 0;
+  
+  //     if (typeof rolls !== 'object'){
+  //         return `value must be an array`
+  
+  //     }
+  // // for (let i = 0; i < rolls.length; i++) {
+  // //      count += rolls[i];
+      
+  // // }
+  // // return count;
+  // for (const roll of rolls) {
+  //     count += roll
+  // }
+  // return count
+  // }
+  
+  // // const rolls = [4,5,6,1]
+  // // console.log(calculateTotal(rolls))
+  
+  // function getMinimumValue(rolls){
+  //     if (rolls.length === 0){
+  //         return `Array must have atleast one value`
+  //     }
+  //     let accumulator = rolls[0]
+  
+  //     for (let i = 1; i < rolls.length; i++) {
+  //         const roll = rolls[i];
+  //         if(roll < accumulator){
+  //             accumulator = roll;
+  //         }
+          
+  //     }
+  //     return accumulator;
+  // }
+  
+  // // const rolls = [4,5,6,1]
+  // // console.log(getMinimumValue(rolls))
+  
+  // function listPlayers(names) {
+  //     // guard clause 1
+  //     if (names.length === 0){
+  //         return `array must have at least one value`
+  //     }
+  
+  //     // guard clause 2
+  //     for (const name of names) {
+  //         if(typeof name !== 'string'){
+  //             return `one of the values is not a string`
+  //         }
+  //     }
+  //     let accumulator = "";
+    
+  //     for (let name of names) {
+  //       accumulator += name + ", "
+  //     }
+    
+  //     return accumulator;
+  //   }
+    
+  //   const players = ["Sebastian", "Kai", "Saanvi"];
+  // //   const players = []
+  //   console.log(listPlayers(players)); //> "Sebastian, Kai, Saanvi,"
+  
+  //   function hasSix(rolls) {
+  //     let accumulator = false;
+    
+  //     for (let roll of rolls) {
+  //       if (roll === 6) {
+  //         accumulator = true;
+  //       }
+  //     }
+    
+  //     return accumulator;
+  //   }
+    
+  // //   const rolls = [4, 5, 3, 1];
+  // //   console.log(hasSix(rolls)); //> true
+  
+  //   function addBonusToAllRolls(rolls) {
+  //     let accumulator = [];
+    
+  //     for (let roll of rolls) {
+  //       let increased = roll + 2;
+  //       accumulator.push(increased);
+  //     }
+    
+  //     return accumulator;
+  //   }
+    
+  // //   const rolls = [4, 5, 6, 1];
+  // //   console.log(addBonusToAllRolls(rolls)); //> [ 6, 7, 8, 1 ];
+  
+  //   function getUniqueRolls(rolls) {
+  //     let accumulator = {};
+    
+  //     for (let roll of rolls) {
+  //       accumulator[roll] = true;
+  //     }
+    
+  //     return accumulator;
+  //   }
+    
+  //   const rolls = [4, 5, 6, 1, 5, 4, 4, 1];
+  //   console.log(getUniqueRolls(rolls));
+  //   //> { '1': true, '4': true, '5': true, '6': true }
+  
+  //   //   Write a function that counts how many of each character are in a given string.
+  // //   Example: "aaabwfbaaaww" 
+  //   function countchars(str){
+  //     // inside function initialize a accumulator and assign it a value of {}
+  //     if (typeof str !== 'string'){
+  //         return `value must be a string`
+  //     }
+  //     let accumulator = {}
+  
+  //     for (let i =0; i <str.length; i++){
+  //         let letter = str[i]
+  //         if(!accumulator[letter]){
+  //             //add the letter as a key and value as 1
+  //             accumulator[letter]= 1
+  //         }else{
+  //             //add 1 to that key
+  //             accumulator[letter] += 1
+  //         }
+  
+  //     }
+  
+  //     return accumulator;
+  //   }
+  
+  //   const count = "aaabwfbaaaww"
+  //   console.log(countchars(count));
